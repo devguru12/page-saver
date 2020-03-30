950bc9be77c5656ba0e8a8c6864f34ee15d265e4
devguru12 <webguru1020@protonmail.com>
2020-03-30 18:35:12 +0900

    Completed

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const sanitizeHtml = require('sanitize-html')
 const write = require('write')
 
 googleIt({'query': 'search'}).then(results => {
-	results.forEach(result => {
-		fetchUrl(result.link, function(error, meta, body) {
-
-		})
-	})
+  results.forEach(result => {
+    fetchUrl(result.link, function(error, meta, body) {
+      if(body) {
+        const text = sanitizeHtml(body.toString(), {
+          allowedTags: [],
+          nonTextTags: [ 'style', 'script', 'textarea', 'noscript', 'xml', 'svg' ]
+        })
+        write.sync('foo.txt', text, { newline: true, flag: 'as' }); 
+      }
+    })
+  })
 }).catch(err => {
   if(err) console.error(err)
 })
