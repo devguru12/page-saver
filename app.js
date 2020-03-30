71f258587448c555a6b3b457d41e1880601862aa
devguru12 <webguru1020@protnmail.com>
2020-03-30 13:46:31 +0900

    Page scraping feature added

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const sanitizeHtml = require('sanitize-html')
 const write = require('write')
 
 googleIt({'query': 'codeigniter'}).then(results => {
-	
+	results.forEach(result => {
+    fetchUrl(result.link, function(error, meta, body) {
+    	
+    })
+  })
 }).catch(err => {
   if(err) console.error(err)
 })
