42c1efa5a0791f430a8687eb24154cf86330d1d3
devguru12 <webguru1020@protonmail.com>
2020-03-30 14:07:25 +0900

    Changed master branch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fetchUrl = require("fetch").fetchUrl
 const sanitizeHtml = require('sanitize-html')
 const write = require('write')
 
-googleIt({'query': 'codeigniter'}).then(results => {
+googleIt({'query': 'search'}).then(results => {
 	
 }).catch(err => {
   if(err) console.error(err)
